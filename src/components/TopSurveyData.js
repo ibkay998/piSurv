26d3b171c700721fa35e79f22cdd6d0001a2b511
ibukunolouwa oyeniyi <ibukun46@gmail.com>
2022-07-27 00:20:36 +0100

    added the react app for pisurv

---
+++ src/components/TopSurveyData.js
@@ -0,0 +1,7 @@
+let SurveyList=[
+    {name:"Market Ownership Understanding of Complex market strategies",description:"cocacola",id:1},
+    {name:"Market Ownership Understanding of Complex market strategies",description:"Apple",id:1},
+    {name:"Market Ownership Understanding of Complex market strategies",description:"Samsung",id:1},
+]
+
+export default SurveyList
---
+++ src/components/historyData.js
@@ -0,0 +1,8 @@
+let HistoryList=[
+    {name:"Market Ownership Understanding of Complex market strategies",completed:"True"},
+    {name:"Market Ownership Understanding of Complex market strategies",completed:"False"},
+    {name:"Market Ownership Understanding of Complex market strategies",completed:"False"},
+    {name:"Market Ownership Understanding of Complex market strategies",completed:"True"},
+]
+
+export default HistoryList
