082120d39a4c30ce65124618c63cde57a93f6395
ibukunolouwa oyeniyi <ibukun46@gmail.com>
2022-08-05 13:45:32 +0100

    fixed landing page

--- src/routes/MainBody.js
+++ src/routes/MainBody.js
@@ -3,17 +3,16 @@ import {BiBookmark} from "react-icons/bi"
 
 function MainBody() {
   return (
-    <div className='grid grid-cols-2 gap-[15%] mt-[9rem] mb-[2rem] '>
-        <div className='flex flex-col ml-3 items-center'>
-            <p className='text-3xl pl-4 font-bold mt-4 '>Fill Surveys And Get Paid</p>
+    <div className='grid grid-cols-1 gap-[15%] mt-[9rem] mb-[2rem] sm:grid-cols-2 '>
+        <div className='flex flex-col ml-3 items-center m-auto justify-center'>
+            <p className='text-2xl leading-6 tracking-tight pl-4 font-bold mt-4 items-center'>Fill Surveys And Get Paid</p>
             <button className=' bg-fuchsia-900 mt-4 text-white rounded-lg hover:bg-fuchsia-300 hover:text-black p-2'><a href="/register"> Register</a>  </button>
             <small className='mt-5 text-center'>Already Registered  <br/> <a href="/login" className='text-lg hover:text-fuchsia-300'>Login</a></small>
         </div>
-        <div className='flex w-[40%] '>
-            <div className='bg-fuchsia-900 items-center hover:bg-transparent hover:border-2 transition duration-300 p-6'>
+        <div className='flex '>
+            <div className='bg-fuchsia-900 items-center hover:bg-transparent hover:border-2 transition duration-300 p-6 flex flex-col items-center'>
                 <BiBookmark className='m-auto'/>
-                <h5>Do you want to Get Paid by FIlling Surveys</h5>
-                <h2>Pisirv</h2>
+                <h5>Do you have surveys that need to be filled by users</h5>
                 <button className=' bg-white mt-4 text-black rounded-lg hover:bg-fuchsia-300  p-2'><a href="/register">Get Started Now</a></button>
             </div>
         </div>
--- src/routes/SliderHeader.js
+++ src/routes/SliderHeader.js
@@ -1,13 +1,32 @@
-import React from 'react'
+import React,{useState} from 'react'
+import {Link} from 'react-router-dom'
+
 
 function SliderHeader() {
+let [open,setOpen] = useState(false)
   return (
     <section id="Home">
         <nav className='flex justify-between border-b-2 border-black p-3'>
             <p className="font-medium text-2xl">PiSurv</p>
             <div className='flex gap-2'>
-                <a href="/login?company" className='p-2 bg-fuchsia-700 hover:bg-fuchsia-200 hover:text-black rounded-md'>Login</a>
-                <a href="/login?user"className='p-2 bg-fuchsia-700 hover:bg-fuchsia-200 hover:text-black rounded-md'>Company Login</a>
+                {/* <a href="/login?company" className='p-2 bg-fuchsia-700 hover:bg-fuchsia-200 hover:text-black rounded-md'>Login</a>
+                <a href="/login?user"className='p-2 bg-fuchsia-700 hover:bg-fuchsia-200 hover:text-black rounded-md'>Company Login</a> */}
+                <div onClick={()=>setOpen(!open)} className="3xl absolute right-8 top-6 cursor-pointer md:hidden">
+                    <ion-icon name={open ? "close":"menu"}></ion-icon>
+                </div>
+                <ul className={` ${open ? "flex flex-col absolute":"hidden"} mt-8 sm:m-auto text-sm gap-2 text-black tracking-tight leading-snug right-6 sm:flex bg-white sm:bg-transparent ease-in`}>
+                   
+                    <li className="">
+                        <Link to={`/login?company`} className="sm:bg-fuchsia-700 sm:p-2 sm:rounded-md text-white rounded-lg hover:bg-fuchsia-300 hover:text-black duration-500 ">Login</Link>
+                    </li>
+                    <li className="">
+                        <Link to={`/login?user`} className="sm:bg-fuchsia-700 sm:p-2 sm:rounded-md  text-white rounded-lg hover:bg-fuchsia-300 hover:text-black duration-500 ">Company Login</Link>
+                    </li>
+
+                        
+                    
+                    
+                </ul>
             </div>
         </nav>
     </section>
